bc86b51d4716ba46ab7660c04af59a31cde6d319
Kumar Tanay <krtanay97@gmail.com>
2026-01-30 12:07:25 +0530

    Update siteMetaData.mjs

--- src/data/siteMetaData.mjs
+++ src/data/siteMetaData.mjs
@@ -1,21 +1,26 @@
 export const siteMetadata = {
-  title: "Kumar Tanay portfolio",
-  author: "Kumar Tanay",
-  headerTitle: "Kumar Tanay",
-  description: "Frontend and Reactjs developer",
-  language: "en-us",
+  title: "Kr Tanay | Full-Stack Developer",
+  author: "Kr Tanay",
+  headerTitle: "Kr Tanay",
+  description:
+    "Personal portfolio of Kr Tanay, a Full-Stack Developer specializing in Next.js, React, Node.js, and modern web technologies. Showcasing projects, skills, and real-world experience.",
+  language: "en-US",
   theme: "light",
-  siteName: "Your Portfolio Name",
-  siteUrl: "https://your-portfolio-domain.com",
-  siteRepo: "https://github.com/SaurabhKumar171/saurabhkumar-v2",
+
+  siteName: "Kr Tanay Portfolio",
+  siteUrl: "https://krtanay007.vercel.app",
+  siteRepo: "https://github.com/krtanay7/tanay-portfolio",
+
   siteLogo: "/favicon.ico",
   image: "/static/homepage.png",
   twitterImage: "/static/homepage.png",
   socialBanner: "/static/homepage.png",
+
   email: "krtanay97@gmail.com",
   github: "https://github.com/krtanay7",
   twitter: "https://x.com/KrTanay007",
   linkedin: "https://www.linkedin.com/in/kumar-tanay97/",
+
   locale: "en-US",
   googleSiteVerification: "YOUR-GOOGLE-VERIFICATION-CODE",
 };
