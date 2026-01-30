c53a08afd9652ece8ce82a98114a0a8d49673c00
Kumar Tanay <krtanay97@gmail.com>
2026-01-30 14:33:33 +0530

    Update footer.tsx

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
           target="_blank"
           className="mb-6 cursor-pointer text-center text-2xl font-bold underline sm:text-4xl lg:text-6xl"
         >
-          <span>kumartanay@gmail.com</span>
+          <span>krtanay97@gmail.com</span>
         </a>
         <div className="flex justify-center">
           <ContactButton />
